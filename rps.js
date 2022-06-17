fc7fb1d3cc405ebc85d5c6767d0d7c29666a0e6d
Ryan Sienerth <delafwod@gmail.com>
2022-06-17 15:52:12 -0400

    Rework the program to be button-focused

--- rps.js
+++ rps.js
@@ -1,11 +1,34 @@
-const newGame = document.querySelector(".newGame");
+const newGame = document.querySelector('.newGame');
 const pScore = document.querySelector(".pScore");
 const cScore = document.querySelector(".cScore");
+const allText = document.querySelector(".allText");
 let computerPick;
 let playerPick;
 let playerPick2;
 let playerScore = 0;
 let computerScore = 0;
+const span = document.createElement('span');
+span.textContent = "Select your choice";
+
+const rockbTN = document.createElement('button');
+rockbTN.textContent = "Rock";
+rockbTN.classList.add('choice');
+span.appendChild(rockbTN);
+
+const paperbTN = document.createElement('button');
+paperbTN.textContent = "Paper";
+paperbTN.classList.add('choice');
+span.appendChild(paperbTN);
+
+const scissorsbTN = document.createElement('button');
+scissorsbTN.textContent = "Scissors";
+scissorsbTN.classList.add('choice');
+span.appendChild(scissorsbTN);
+
+const buttons = document.querySelectorAll(".choice");
+
+const par1 = document.createElement('p');
+par1.classList.add('update');
 
 function computerPlay() {
     computerPick = Math.floor(Math.random()*3)+1
@@ -26,7 +49,6 @@ function playerPlay(playerPick) {
 return playerPick2;
 }
 function gamePrep() {
-playerPick = prompt("Pick Rock, Paper, Or Scissors");
 playerPick = playerPick.toLowerCase();
 if (playerPick!= "rock" && playerPick!="paper" && playerPick!="scissors"){
     alert("Invalid choice. Pick Again");}
@@ -35,44 +57,60 @@ if (playerPick!= "rock" && playerPick!="paper" && playerPick!="scissors"){
     } 
 }
 
-function playRound(playerPick) {
+function playRound(playerPick2) {
     computerPlay();
-    if(playerPick==1 && computerPick==3){
+    if(playerPick2==1 && computerPick==3){
         playerScore++;
-        alert("You won! Player: "+playerScore+" Computer: "+computerScore)
+        pScore.textContent = "Player: " +playerScore;
+        cScore.textContent = "Computer: "+computerScore;
+        par1.textContent = " You Won! ";
     }
-    else if(computerPick==1 && playerPick==3) {
+    else if(computerPick==1 && playerPick2==3) {
         computerScore++;
-        alert("You lose! Player: "+playerScore+" Computer: "+computerScore)
+        pScore.textContent = "Player: " +playerScore;
+        cScore.textContent = "Computer: "+computerScore;
+        par1.textContent = " You Lost! ";
     }
-    else if(computerPick>playerPick) {
+    else if(computerPick>playerPick2) {
         computerScore++;
-        alert("You lose! Player: "+playerScore+" Computer: "+computerScore)
+        pScore.textContent = "Player: " +playerScore;
+        cScore.textContent = "Computer: "+computerScore;
+        par1.textContent = " You Lost! ";
     }
-    else if(playerPick>computerPick) {
+    else if(playerPick2>computerPick) {
         playerScore++;
-        alert("You won! Player: "+playerScore+" Computer: "+computerScore)
+        pScore.textContent = "Player: " +playerScore;
+        cScore.textContent = "Computer: "+computerScore;
+        par1.textContent = " You Won! ";
     }
     else
-        alert("It's a tie. Player: "+playerScore+" Computer: "+computerScore);
+    par1.textContent = " It's a tie. "
+    pScore.textContent = "Player: " +playerScore;
+    cScore.textContent = "Computer: "+computerScore;
     
 }
 
 function declareWinner(playerScore,computerScore){
-    if(playerScore>computerScore){
-    pScore.textContent = "Player: Congratulations! You Won!";
-    cScore.textContent = "Computer: Loser";
+    if(playerScore==5 || computerScore==5){
+        if(playerScore>computerScore){
+        pScore.textContent = "Player: Congratulations! You Won!";
+         cScore.textContent = "Computer: Loser";
+         allText.removeChild(span);
+         par1.textContent = " You Won! ";
     }
-    else if(computerScore>playerScore){
-    pScore.textContent = "Player: Loser";
-    cScore.textContent = "Computer: Congratulations! You Won!";
+         else if(computerScore>playerScore){
+         pScore.textContent = "Player: Loser";
+        cScore.textContent = "Computer: Congratulations! You Won!";
+        allText.removeChild(span);
+        par1.textContent = " You Lost! ";
     }
-    else{
+         else{
         pScore.textContent = "DRAW";
-    cScore.textContent = "DRAW";
+         cScore.textContent = "DRAW";
+    }
     }
 }
-function playGame(){
+/*function playGame(){
     playerScore = 0;
     computerScore = 0;
     pScore.textContent = `Player:${playerScore}`;
@@ -85,4 +123,41 @@ function playGame(){
     cScore.textContent = `Computer:${computerScore}`;
     }
     declareWinner(playerScore,computerScore);
-}
+}*/
+
+newGame.addEventListener('click',()=> {
+    playerScore = 0;
+    computerScore = 0;
+    pScore.textContent = `Player:${playerScore}`;
+    cScore.textContent = `Computer:${computerScore}`;
+    allText.appendChild(span);
+    allText.appendChild(par1);
+})
+rockbTN.addEventListener('click',()=>{
+        playerPick = "rock";
+        playerPlay(playerPick);
+        computerPlay();
+        console.log(playerPick2);
+        console.log(computerPick);
+        playRound(playerPick2);
+        declareWinner(playerScore,computerScore);
+    })
+
+paperbTN.addEventListener('click',()=>{
+        playerPick = "paper";
+        playerPlay(playerPick);
+        computerPlay();
+        console.log(playerPick2);
+        console.log(computerPick);
+        playRound(playerPick2);
+        declareWinner(playerScore,computerScore);
+    })
+
+scissorsbTN.addEventListener('click',()=>{
+        playerPick = "scissors";
+        playerPlay(playerPick);
+        console.log(playerPick2);
+        console.log(computerPick);
+        playRound(playerPick2);
+        declareWinner(playerScore,computerScore);
+    })
